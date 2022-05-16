46ee4b64957eee1faabdf31076faaa085a2fb175
Ferdinand Saurenbach <ferdinand161@outlook.de>
2022-05-16 18:19:52 +0200

    Added an explanation of the controls (#1)

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import ursina
 from ursina import *
 from ursina import curve
 
-
 app = Ursina()
+window.title = 'My Game'  # The window title
+window.borderless = False  # Show a border
+window.fullscreen = True
+window.exit_button.visible = False  # Do not show the in-game red X that loses the window
+window.fps_counter.enabled = True
+window.fps_counter.scale_x = 4
+window.fps_counter.scale_y = 4
+window.fps_counter.x = 0.9
+window.fps_counter.color = color.red
 gun = Entity(model='cube', parent=camera, position=(.5, -.25, .25), scale=(.3, .2, 1), origin_z=-.5, color=color.red,
              on_cooldown=False)
 
 gun.muzzle_flash = Entity(parent=gun, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
 EnemyGun = Entity(model='cube', parent=camera, position=(.5, -.25, .25), scale=(.3, .2, 1), origin_z=-.5,
-                  color=color.red,
+                  color=color.blue,
                   on_cooldown=False)
 EnemyGun.muzzle_flash = Entity(parent=gun, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
 
@@ -27,9 +35,11 @@ def shootEnemy():
 		from ursina.prefabs.ursfx import ursfx
 		ursfx([(0.0, 0.0), (0.1, 0.9), (0.15, 0.75), (0.3, 0.14), (0.6, 0.0)], volume=0.5, wave='noise',
 		      pitch=random.uniform(-13, -12), pitch_change=-12, speed=3.0)
-		# if mouse.hovered_entity and hasattr(mouse.hovered_entity, 'hp'):
-		# 	mouse.hovered_entity.hp -= 10
-		# 	mouse.hovered_entity.blink(color.red)
+
+
+# if mouse.hovered_entity and hasattr(mouse.hovered_entity, 'hp'):
+# 	mouse.hovered_entity.hp -= 10
+# 	mouse.hovered_entity.blink(color.red)
 
 
 def shoot():
@@ -51,6 +61,10 @@ class Player(Entity):
 	def __init__(self, **kwargs):
 		self.cursor = Entity(parent=camera.ui, model='quad', color=color.pink, scale=.008, rotation_z=45)
 		super().__init__()
+		self.health_bar = Entity(parent=self, y=1.2, model='cube', color=color.red, world_scale=(1.5, .1, .1))
+		self.max_hp = 50
+		self.hp = self.max_hp
+
 		self.speed = 5
 		self.height = 2
 		self.camera_pivot = Entity(parent=self, y=self.height)
@@ -66,21 +80,20 @@ class Player(Entity):
 		self.grounded = False
 		self.jump_height = 2
 		self.jump_up_duration = .5
-		self.fall_after = .35  # will interrupt jump up
+		self.fall_after = .35
 		self.jumping = False
 		self.air_time = 0
 
 		for key, value in kwargs.items():
 			setattr(self, key, value)
 
-		# make sure we don't fall through the ground if we start inside it
 		if self.gravity:
 			ray = ursina.raycast(self.world_position + (0, self.height, 0), self.down, ignore=(self,))
 			if ray.hit:
 				self.y = ray.world_point.y
 
 	def update(self):
-
+		self.health_bar.alpha = max(0, self.health_bar.alpha - time.dt)
 		self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
 
 		self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[0]
@@ -99,22 +112,18 @@ class Player(Entity):
 			self.position += self.direction * self.speed * time.dt
 
 		if self.gravity:
-			# gravity
 			ray = ursina.raycast(self.world_position + (0, self.height, 0), self.down, ignore=(self,))
-			# ray = boxcast(self.world_position+(0,2,0), self.down, ignore=(self,))
 
 			if ray.distance <= self.height + .1:
 				if not self.grounded:
 					self.land()
 				self.grounded = True
-				# make sure it's not a wall and that the point is not too far up
-				if ray.world_normal.y > .7 and ray.world_point.y - self.world_y < .5:  # walk up slope
+				if ray.world_normal.y > .7 and ray.world_point.y - self.world_y < .5:
 					self.y = ray.world_point[1]
 				return
 			else:
 				self.grounded = False
 
-			# if not on ground and not on way up in jump, fall
 			self.y -= min(self.air_time, ray.distance - .05) * time.dt * 100
 			self.air_time += time.dt * .25 * self.gravity
 
@@ -142,7 +151,6 @@ class Player(Entity):
 		invoke(self.start_fall, delay=self.fall_after)
 
 	def land(self):
-		# print('land')
 		self.air_time = 0
 		self.grounded = True
 
@@ -154,12 +162,27 @@ class Player(Entity):
 		mouse.locked = False
 		self.cursor.enabled = False
 
+	@property
+	def hp(self):
+		return self._hp
+
+	@hp.setter
+	def hp(self, value):
+		self._hp = value
+		if value <= 0:
+			destroy(self)
+			return
+
+		self.health_bar.world_scale_x = self.hp / self.max_hp * 1.5
+		self.health_bar.alpha = 1
+
+
 class Enemy(Entity):
 	def __init__(self, **kwargs):
 		super().__init__(parent=shootables_parent, model='cube', texture='zombie', scale=2.5, origin_y=-.5,
 		                 collider='box', **kwargs)
 		self.health_bar = Entity(parent=self, y=1.2, model='cube', color=color.red, world_scale=(1.5, .1, .1))
-		self.max_hp = 100
+		self.max_hp = 50
 		self.hp = self.max_hp
 
 	def update(self):
@@ -191,13 +214,10 @@ class Enemy(Entity):
 		self.health_bar.alpha = 1
 
 
-# Enemy()
 enemy1 = Enemy()
 
 EnemyGun.parent = enemy1
 if __name__ == '__main__':
-	# window.vsync = False
-	# Sky(color=color.gray)
 	ground = Entity(model='plane', scale=(100, 1, 100), color=color.yellow.tint(-.2), texture='white_cube',
 	                texture_scale=(100, 100), collider='box')
 	e = Entity(model='cube', scale=(1, 5, 10), x=2, y=.01, rotation_y=45, collider='box', texture='white_cube')
@@ -212,11 +232,17 @@ if __name__ == '__main__':
 	               texture_scale=(8, 8))
 	slope = Entity(model='cube', collider='box', position=(5, 0, 10), scale=6, rotation=(80, 0, 0), texture='brick',
 	               texture_scale=(8, 8))
-	# hill = Entity(model='sphere', position=(20,-10,10), scale=(25,25,25), collider='sphere', color=color.green)
-	# hill = Entity(model='sphere', position=(20,-0,10), scale=(25,25,25), collider='mesh', color=color.green)
-	# from ursina.shaders import basic_lighting_shader
-	# for e in scene.entities:
-	#     e.shader = basic_lighting_shader
-
-	# player.add_script(NoclipMode())
+	Text.size = 0.05
+	Text.default_resolution = 1080 * Text.size
+	info = Text(text="Controls:\n"
+	                 "Move forward: W\n"
+	                 "Move backwards: A\n"
+	                 "Move left: A\n"
+	                 "Move right: D\n"
+	                 "Jump: space\n"
+	                 "Fire: right mouse")
+	info.x = -0.85
+	info.y = 0.465
+	info.background = True
+	info.visible = True
 	app.run()
